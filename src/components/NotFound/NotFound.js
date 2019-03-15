ca5bb6ba7ab35fee4912e0d9caff62593e47ecdf
Daniel Waters <danwater1@gmail.com>
2019-03-15 11:04:34 -0700

    Finishes Not Found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import Layout from "../Layout/Layout";
+import { Container, Col, Row } from "reactstrap";
+
+import styles from "./NotFound.module.css";
 
 const NotFound = () => (
-  <div>
-    <h1>Oops, page not found!</h1>
-  </div>
-)
+  <Layout>
+    <div className={styles.notFoundBackground}>
+      <Container className={styles.notFoundContainer}>
+        <div>
+          <p className={styles.errorHeader}>Error 404</p>
+        </div>
+        <div>
+          <h2>Oops! It looks like this page was knocked out!</h2>
+          <h4>Please check the URL and try again.</h4>
+        </div>
+      </Container>
+    </div>
+  </Layout>
+);
 
 export default NotFound;
