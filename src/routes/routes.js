e3a620666b5e4b9a0514f253f84191e27ef553f3
Daniel Waters <danwater1@gmail.com>
2019-03-14 19:15:10 -0700

    Adds forgot password and create account routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginPage from "../components/LoginPage/LoginPage";
 import LandingPage from "../components/LandingPage/LandingPage";
 import NotFound from "../components/NotFound/NotFound";
+import CreateAccountPage from '../components/CreateAccountPage/CreateAccountPage';
+import ForgotPasswordPage from '../components/ForgotPasswordPage/ForgotPasswordPage';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LandingPage} />
       <Route path="/account/login" component={LoginPage} />
+      <Route path="/account/create" component={CreateAccountPage} />
+      <Route path="/account/forgot" component={ForgotPasswordPage} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
