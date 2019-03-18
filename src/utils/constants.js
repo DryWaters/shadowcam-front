133c63d15fd6113941012482095792dba5f2532f
Daniel Waters <danwater1@gmail.com>
2019-03-17 18:56:54 -0700

    Exports constants to single file

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const MAX_WEIGHT = 400;
+export const MIN_WEIGHT = 40;
+export const MAX_HEIGHT = 100;
+export const MIN_HEIGHT = 40;
+export const MIN_YEAR = 1900;
+export const MAX_YEAR = 2007;
+export const MIN_PASSWORD_LENGTH = 8;
