1210b8f01008fc346c7866551f841a7c29868854
Daniel Waters <danwater1@gmail.com>
2019-03-24 17:20:53 -0700

    Displays webcam and stops recording

--- src/pages/NewRecordingPage/NewRecordingPage.js
+++ src/pages/NewRecordingPage/NewRecordingPage.js
@@ -10,24 +10,69 @@ export class NewRecordingPage extends Component {
   constructor(props) {
     super(props);
     this.videoRef = React.createRef();
+    this.state = {
+      time: 0,
+      totalPunches: 0,
+      numJabs: 0,
+      numHooks: 0,
+      isRecording: false
+    };
   }
 
   componentDidMount() {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
-      .then(localMediaStream => {
-        this.videoRef.current.srcObject = localMediaStream;
+      .then(stream => {
+        this.videoRef.current.srcObject = stream;
         this.videoRef.current.play();
       });
+  }
+
+  componentWillUnmount() {
+    const stream = this.videoRef.current.srcObject;
+    const tracks = stream.getTracks();
+
+    tracks.forEach(track => track.stop());
+    this.videoRef.current.srcObject = null;
+  }
+
+  toggleRecording = () => {
+    this.setState({
+      isRecording: !this.state.isRecording
+    })
   }
   render() {
     return (
       <Layout>
         <Container className={styles.newRecordingContainer}>
-          <Row className={styles.videoContainer}>
+          <Row>
             <Col>
               <video className={styles.video} ref={this.videoRef} />
             </Col>
+          </Row>
+          <Row className={styles.videoButtons}>
+            <button className={styles.button} onClick={this.toggleRecording}>
+              <div className={this.state.isRecording ? styles.stop : styles.record} />
+            </button>
+          </Row>
+          <Row className={styles.spacer}>
+            <h2>Statistics</h2>
+          </Row>
+          <Row className={styles.spacer}>
+            <Col>Total Time:</Col>
+            <Col>{this.state.time}</Col>
+          </Row>
+          <Row className={styles.spacer}>
+            <Col>Number of Punches</Col>
+            <Col>{this.state.totalPunches}</Col>
+          </Row>
+          <Row className={styles.spacer}>
+            <Col>Number of Jabs</Col>
+            <Col>{this.state.numJabs}</Col>
+          </Row>
+          <Row className={styles.spacer}>
+            <Col>Number of Hooks</Col>
+            <Col>{this.state.numHooks}</Col>
           </Row>
         </Container>
       </Layout>
