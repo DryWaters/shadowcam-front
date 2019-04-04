61c77950ad0ccbff3ddfe00a21f5633fd7ea305a
Daniel Waters <danwater1@gmail.com>
2019-04-04 11:39:06 -0700

    Creates new workout page

---
+++ src/pages/NewWorkoutPage/NewWorkoutPage.js
@@ -0,0 +1,185 @@
+import React, { Component } from "react";
+import {
+  Row,
+  Container,
+  Col,
+  Form,
+  Input,
+  Button,
+  Label,
+  FormGroup
+} from "reactstrap";
+import { connect } from "react-redux";
+import Layout from "../../components/Layout/Layout";
+
+import styles from "./NewWorkoutPage.module.css";
+
+export class NewWorkoutPage extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+    this.state = {
+      width: 640,
+      height: 480,
+      totalTimeMin: 0,
+      totalTimeSec: 0,
+      intervalTimeMin: 0,
+      intervalTimeSec: 0,
+      numberInterval: 0
+    };
+  }
+
+  async componentDidMount() {
+    await this.loadVideo();
+  }
+
+  componentWillUnmount() {
+    this.videoRef.current.pause();
+    const tracks = this.currentStream.getTracks();
+    tracks.forEach(track => track.stop());
+    this.videoRef.current = null;
+  }
+
+  setupCamera = async () => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      alert("Newer browser required to use ShadowCam");
+    }
+    this.videoRef.current.width = this.state.width;
+    this.videoRef.current.height = this.state.height;
+    this.currentStream = await navigator.mediaDevices.getUserMedia({
+      mimeType: "video/webm; codecs=vp9",
+      audio: false,
+      video: {
+        facingMode: "user",
+        width: this.state.width,
+        height: this.state.height
+      }
+    });
+
+    this.videoRef.current.srcObject = this.currentStream;
+    return new Promise(resolve => {
+      this.videoRef.current.onloadedmetadata = () => {
+        resolve(this.videoRef.current);
+      };
+    });
+  };
+
+  loadVideo = async () => {
+    this.videoRef.current = await this.setupCamera();
+    if (this.videoRef.current) {
+      this.videoRef.current.play();
+    }
+  };
+
+  handleInputChange = event => {};
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Container className={styles.newWorkoutContainer}>
+          <Row>
+            <Col>
+              <video
+                ref={this.videoRef}
+                srcobject={this.currentStream}
+                className={styles.video}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form onSubmit={this.handleSubmit} className={styles.form}>
+                <div className={styles.timeCategory}>Total time</div>
+                <FormGroup row className={styles.spacer}>
+                  <Label sm="2" for="totalTimeMin">
+                    Minutes
+                  </Label>
+                  <Col sm="4" md="3">
+                    <Input
+                      type="number"
+                      min={this.state.minTime}
+                      id="totalTimeMin"
+                      value={this.state.totalTimeMin}
+                      onChange={this.handleInputChange}
+                      placeholder="Total workout time minutes"
+                    />
+                  </Col>
+                  <Label sm="2" for="totalTimeSec">
+                    Seconds
+                  </Label>
+                  <Col sm="4" md="3">
+                    <Input
+                      type="number"
+                      min="0"
+                      max="59"
+                      id="totalTimeSec"
+                      value={this.state.totalTimeSec}
+                      onChange={this.handleInputChange}
+                      placeholder="Total workout time seconds"
+                    />
+                  </Col>
+                </FormGroup>
+                <div className={styles.timeCategory}>Interval time</div>
+                <FormGroup row className={styles.spacer}>
+                  <Label sm="2" for="intervalTimeMin">
+                    Minutes
+                  </Label>
+                  <Col sm="4" md="3">
+                    <Input
+                      type="number"
+                      min="0"
+                      id="intervalTimeMin"
+                      value={this.state.intervalTimeMin}
+                      onChange={this.handleInputChange}
+                      placeholder="Total workout time minutes"
+                    />
+                  </Col>
+                  <Label sm="2" for="intervalTimeSec">
+                    Seconds
+                  </Label>
+                  <Col sm="4" md="3">
+                    <Input
+                      type="number"
+                      min="0"
+                      max="59"
+                      id="intervalTimeSec"
+                      value={this.state.intervalTimeSec}
+                      onChange={this.handleInputChange}
+                      placeholder="Total workout time seconds"
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row className={styles.spacer}>
+                  <Label sm="6" xl="6" for="numberInterval">
+                    Number of Intervals
+                  </Label>
+                  <Col sm="4">
+                    <Input
+                      type="number"
+                      min="0"
+                      id="numberInterval"
+                      value={this.state.numberInterval}
+                      onChange={this.handleInputChange}
+                      placeholder="Number of Intervals"
+                    />
+                  </Col>
+                </FormGroup>
+                <Row className={styles.spacer}>
+                  <Col className={styles.submitButtonContainer}>
+                    <Button>Start Workout!</Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
+    );
+  }
+}
+
+export default connect()(NewWorkoutPage);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,6 +8,7 @@ import CreateAccountPage from '../pages/CreateAccountPage/CreateAccountPage';
 import ProfilePage from '../pages/ProfilePage/ProfilePage';
 import NewRecordingPage from '../pages/NewRecordingPage/NewRecordingPage';
 import PastWorkoutsPage from '../pages/PastWorkoutsPage/PastWorkoutsPage';
+import NewWorkoutPage from '../pages/NewWorkoutPage/NewWorkoutPage';
 
 
 const Routes = props => (
@@ -17,7 +18,8 @@ const Routes = props => (
       <Route path="/account/login" render={() => !props.isAuth ? <LoginPage /> : <Redirect to="/workouts/pastWorkouts" />} />
       <Route path="/account/create" render={() => !props.isAuth ? <CreateAccountPage /> : <Redirect to="/workouts/newWorkout" />} />} />
       <Route path="/account/profile" render={() => props.isAuth ? <ProfilePage /> : <Redirect to="/" />} />
-      <Route path="/workouts/newWorkout" render={() => props.isAuth ? <NewRecordingPage /> : <Redirect to="/" />} />
+      <Route path="/workouts/newRecording" render={() => props.isAuth ? <NewRecordingPage /> : <Redirect to="/" />} />
+      <Route path="/workouts/newWorkout" render={() => props.isAuth ? <NewWorkoutPage /> : <Redirect to="/" />} />
       <Route path="/workouts/pastWorkouts" render={() => props.isAuth ? <PastWorkoutsPage /> : <Redirect to="/" />} />
       <Route component={NotFoundPage} />
     </Switch>
