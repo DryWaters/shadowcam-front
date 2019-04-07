30a390423431488b7b65fee709c891687cbab591
Daniel Waters <danwater1@gmail.com>
2019-04-07 10:56:57 -0700

    Integrated the /workouts call into the past recordings

--- src/pages/PastWorkoutsPage/PastWorkoutsPage.js
+++ src/pages/PastWorkoutsPage/PastWorkoutsPage.js
@@ -3,59 +3,136 @@ import { connect } from "react-redux";
 import { Container, Row, Col } from "reactstrap";
 import Moment from "moment";
 import Layout from "../../components/Layout/Layout";
+import { loading, notLoading } from "../../store/actions/ui";
+import { formatTimeFromSeconds } from "../../utils/utils";
+import loadingSpinner from "../../assets/images/loading-spinner.gif";
 
 import styles from "./PastWorkoutsPage.module.css";
 
 export class PastWorkoutsPage extends Component {
   state = {
-    currentVideo: null,
-    videos: [
-      {
-        videoName: "Test Movie",
-        src: "https://shadowcam-back.herokuapp.com/videos/testmovie.webm",
-        timeStamp: new Moment().format(),
-        screenShot: "https://shadowcam-back.herokuapp.com/screenshots/testmovie.png"
-      },
-      {
-        videoName: "Test Movie2",
-        src: "https://shadowcam-back.herokuapp.com/videos/testmovie2.webm",
-        timeStamp: new Moment().subtract(7, "days").format(),
-        screenShot: "https://shadowcam-back.herokuapp.com/screenshots/testmovie2.png"
-      }
-    ]
+    currentWorkout: null,
+    workouts: null
   };
 
   componentDidMount() {
-    // fetch user's videos
+    this.props.loading();
+
+    let url;
+
+    if (process.env.REACT_APP_TEST) {
+      url = "http://localhost:3000/workouts";
+    } else {
+      url = `https://shadowcam-back.herokuapp.com/workouts`;
+    }
+
+    return fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.props.token}`
+      },
+      mode: "cors"
+    })
+      .then(res => {
+        if (res.status === 401) {
+          return Promise.reject("unathorized");
+        } else {
+          return res.json();
+        }
+      })
+      .then(parsedRes => {
+        this.props.notLoading();
+        if (parsedRes.status === "ok") {
+          this.setState({
+            workouts: parsedRes.workouts
+          });
+        } else {
+          return Promise.reject(parsedRes);
+        }
+      })
+      .catch(error => {
+        this.props.notLoading();
+        if (error === "unathorized") {
+          alert("Please log back in!");
+        } else
+          alert(
+            "Unable to connect to server.  Please check internet connection." +
+              error.message
+          );
+      });
   }
 
-  handleClickWorkout = timeStamp => {
-    const correctVideo = this.state.videos.filter(
-      video => video.timeStamp === timeStamp
-    );
-    this.setState({
-      currentVideo: correctVideo[0].src
-    });
+  handleClickWorkout = id => {
+    let url;
+
+    if (process.env.REACT_APP_TEST) {
+      url = `"http://localhost:3000/workouts/${id}`;
+    } else {
+      url = `https://shadowcam-back.herokuapp.com/workouts/${id}`;
+    }
+
+    return fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.props.token}`
+      },
+      mode: "cors"
+    })
+      .then(res => {
+        if (res.status === 401) {
+          return Promise.reject("unathorized");
+        } else {
+          return res.json();
+        }
+      })
+      .then(parsedRes => {
+        this.props.notLoading();
+        if (parsedRes.status === "ok") {
+          this.setState({
+            currentWorkout: parsedRes.workout
+          });
+        } else {
+          return Promise.reject(parsedRes);
+        }
+      })
+      .catch(error => {
+        this.props.notLoading();
+        if (error === "unathorized") {
+          alert("Please log back in!");
+        } else
+          alert(
+            "Unable to connect to server.  Please check internet connection." +
+              error.message
+          );
+      });
   };
 
   render() {
     const displayPastWorkouts = () => {
-      return this.state.videos.map(video => {
+      if (!this.state.workouts) {
+        return (
+          <Row className={styles.spinnerContainer}>
+            <img
+              src={loadingSpinner}
+              alt="Loading Spinner"
+              className={styles.loadingSpinner}
+            />
+          </Row>
+        );
+      }
+
+      return this.state.workouts.map(workout => {
         return (
           <Row
             className={styles.workout}
-            key={video.timeStamp}
-            onClick={() => this.handleClickWorkout(video.timeStamp)}
+            key={workout.work_id}
+            onClick={() => this.handleClickWorkout(workout.work_id)}
           >
-            <Col>
-              <img
-                className={styles.screenShot}
-                src={video.screenShot}
-                alt={video.videoName}
-              />
-            </Col>
-            <Col>{video.videoName}</Col>
-            <Col>{Moment(video.timeStamp).format("M/D/YY H:m:s a")}</Col>
+            <Col>{workout.work_id}</Col>
+            <Col>{Moment(workout.recording_date).format("M/D/YY H:m:s a")}</Col>
+            <Col>{formatTimeFromSeconds(workout.workout_length)}</Col>
+            <Col>{workout.num_of_intervals}</Col>
+            <Col>{workout.interval_length}</Col>
           </Row>
         );
       });
@@ -101,6 +178,13 @@ export class PastWorkoutsPage extends Component {
           <Row className={styles.spacer}>
             <h2>Workouts</h2>
           </Row>
+          <Row className={styles.workoutsHeader}>
+            <Col>ID</Col>
+            <Col>Date</Col>
+            <Col>Length</Col>
+            <Col>Number of Intervals</Col>
+            <Col>Interval Length</Col>
+          </Row>
           {displayPastWorkouts()}
         </Container>
       </Layout>
@@ -108,4 +192,17 @@ export class PastWorkoutsPage extends Component {
   }
 }
 
-export default connect()(PastWorkoutsPage);
+const mapDispatchToProps = dispatch => ({
+  loading: () => dispatch(loading()),
+  notLoading: () => dispatch(notLoading())
+});
+
+const mapStateToProps = state => ({
+  isLoading: state.ui.isLoading,
+  token: state.user.token
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PastWorkoutsPage);
