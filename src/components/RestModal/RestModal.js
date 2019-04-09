eec94b892db80ab92de218f42a6e18340ed3ddba
Daniel Waters <danwater1@gmail.com>
2019-04-08 18:08:55 -0700

    Removed the ability to break rest timer early to stop from messing up with total time

--- src/components/RestModal/RestModal.js
+++ src/components/RestModal/RestModal.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { Modal, ModalBody, ModalHeader } from "reactstrap";
 
 const RestModal = props => (
   <div>
-    <Modal isOpen toggle={props.stopRest}>
+    <Modal isOpen>
       <ModalHeader>Take a rest!</ModalHeader>
       <ModalBody>
         <div>You deserve a break!</div>
         <div>Take a breather for another {props.restTime} seconds!</div>
       </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={props.stopRest}>
-          Cancel Break
-        </Button>
-      </ModalFooter>
     </Modal>
   </div>
 );
